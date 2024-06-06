624e1f10c076577b98ec21e9061c2e024f64e05f
Eva Garcia <evagarciaclemente96@gmail.com>
2024-06-06 12:48:09 +0200

    usuario añadido en la respuesta del login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -39,7 +39,7 @@ const UserController = {
             if (user.tokens.length > 4) user.tokens.shift();
             user.tokens.push(token);
             await user.save();
-            res.send({ message: 'Bienvenid@ ' + user.name, token });
+            res.send({ message: 'Bienvenid@ ' + user.name, token, user });
         } catch (error) {
             console.error(error);
             res.status(500).send(error)
