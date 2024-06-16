35278ae11fe97fd92e980bca950274dcd24e423c
Eva Garcia <evagarciaclemente96@gmail.com>
2024-06-16 12:47:07 +0200

    post

--- models/Post.js
+++ models/Post.js
@@ -1,35 +1,3 @@
-// const mongoose = require('mongoose');
-// const ObjectId = mongoose.SchemaTypes.ObjectId;
-
-
-// const PostSchema = new mongoose.Schema({
-//     title:{
-//         type:String,
-//         required: [true, "Por favor rellene todos los campos"]
-//     },    
-//     body:{ 
-//         type:String,
-//         required: [true, "Por favor rellene todos los campos"]
-//     },    
-//     userId:{type: ObjectId, ref:"User"},
-//     commentIds: [{type: ObjectId, ref: "Comment"}],
-//     likes:[{type: ObjectId, ref:"User"}],
-//     dislikes:[{type: ObjectId, ref:"User"}],
-// }, { timestamps: true });
-
-
-// PostSchema.methods.toJSON = function() {
-//     const user = this._doc;
-//     delete user.tokens;
-//     delete user.password;
-//     return user;
-// }
-
-    
-// const Post = mongoose.model('Post', PostSchema);
-
-// module.exports = Post;
-
 const mongoose = require('mongoose');
 const ObjectId = mongoose.SchemaTypes.ObjectId;
 
